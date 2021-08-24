f23f41ef1cd97fd1d0eea7dbb3232c1caf3ff912
Kanri <yiwen_chan@foxmail.com>
2021-08-24 23:56:15 +0800

    update

--- src/slowjson.lua
+++ src/slowjson.lua
@@ -1,80 +1,100 @@
-a = '{ "哈哈哈":222,"123":666,"234":[{123:234},{234:234}]}'
+a = '{"哈哈哈":222,"123":666,"234":[{123:234},{234:234}]}'
 
-function parse(text)
+function load(s)
     local temp = ""
     local deep = 0
     local tree = {}
-    for i = 1, #text do
-        current = string.sub(text, i, i)
-        local state = {
-            ["{"] = function()
-                deep = deep + 1
-                if tree[deep] == nil then
-                    table.insert(tree, {})
-                end
-                temp = ""
-            end,
-            ["}"] = function()
-                table.insert(tree[deep], temp)
-                cur = tree[deep]
-                now = {}
-                for j = 1, #cur - 1, 2 do
-                    key = cur[j]
-                    val = cur[j + 1]
-                    now[key] = val
-                end
-                table.remove(tree, deep)
-                deep = deep - 1
-                if deep == 0 then
-                    return now
-                end
-                tree[deep][#tree[deep]] = now
-                temp = ""
-            end,
-            ["["] = function()
-                table.insert(tree[deep], temp)
-                deep = deep + 1
-                if tree[deep] == nil then
-                    table.insert(tree, {})
-                end
-                temp = ""
-            end,
-            ["]"] = function()
-                table.insert(tree[deep], temp)
-                now = tree[deep]
-
-                table.remove(tree, deep)
-                deep = deep - 1
-                if deep == 0 then
-                    return now
-                end
-                tree[deep][#tree[deep]] = now
-                temp = ""
-            end,
-            [":"] = function()
-                table.insert(tree[deep], temp)
-                temp = ""
-            end,
-            [","] = function()
-                table.insert(tree[deep], temp)
-                temp = ""
-            end,
-            ["\""] = function()
-                table.insert(tree[deep], temp)
-                temp = ""
-            end,
-            ["\\"] = function()
-                -- skip
+    local state = {
+        ["{"] = function()
+            deep = deep + 1
+            if tree[deep] == nil then
+                table.insert(tree, {})
             end
-        }
-        if state[current] == nil then
-            temp = temp .. current
-            print(temp)
-        else
+            temp = ""
+        end,
+        ["}"] = function()
+            table.insert(tree[deep], temp)
+            cur = tree[deep]
+            now = {}
+            for j = 1, #cur - 1, 2 do
+                key = cur[j]
+                val = cur[j + 1]
+                now[key] = val
+            end
+            table.remove(tree, deep)
+            deep = deep - 1
+            if deep == 0 then
+                return now
+            end
+            tree[deep][#tree[deep]] = now
+            temp = ""
+        end,
+        ["["] = function()
+            table.insert(tree[deep], temp)
+            deep = deep + 1
+            if tree[deep] == nil then
+                table.insert(tree, {})
+            end
+            temp = ""
+        end,
+        ["]"] = function()
+            table.insert(tree[deep], temp)
+            now = tree[deep]
+
+            table.remove(tree, deep)
+            deep = deep - 1
+            if deep == 0 then
+                return now
+            end
+            tree[deep][#tree[deep]] = now
+            temp = ""
+        end,
+        [":"] = function()
+            table.insert(tree[deep], temp)
+            temp = ""
+        end,
+        [","] = function()
+            table.insert(tree[deep], temp)
+            temp = ""
+        end,
+        ["\""] = function()
+            table.insert(tree[deep], temp)
+            temp = ""
+        end,
+        ["\\"] = function()
+            -- skip
+        end
+    }
+    for i = 1, #s do
+        current = string.sub(s, i, i)
+        if state[current] ~= nil then
             ret = state[current]()
             if ret ~= nil then
                 return ret
             end
+        else
+            temp = temp .. current
+        end
+    end
+end
+
+function dump(obj)
+    local deep = 0
+    local index = 0
+    local times = {}
+    local cur = obj
+    local ret = ""
+    ::label::
+    do
+        for k, v in pairs(cur) do
+            if type(v) == "string" then
+                ret = ret .. "\"" .. k .. "\"" .. ": " .. "\"" .. v .. "\""
+            elseif type(v) == "number" then
+                ret = ret .. "\"" .. k .. "\"" .. ": " .. v
+            elseif type(v) == "table" then
+                
+            end
+            goto label
         end
     end
 end
